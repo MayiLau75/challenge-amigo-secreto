9c8017f1fbd739d90c1a5b4850135de875d43911
Mayi Lau <contactoverity@gmail.com>
2025-03-02 22:14:39 -0500

    actualiza

--- app.js
+++ app.js
@@ -1,38 +1,97 @@
 let amigos = [];
+let listaSorteados = [];
+let amigoSorteado = 0;
 
 function agregarAmigo() {
-    nombreAmigos = document.getElementById("amigo").value;
-    console.log(nombreAmigos);
+    let nombreAmigos = document.getElementById("amigo").value;
+    let regex = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/;
     if (nombreAmigos === "") {
-        alert("Por favor, inserte un nombre.");
-        return;
+        alert("Por favor, ingrese un nombre.");
+    } else if (amigos.includes(nombreAmigos)) {
+        alert('Ese nombre ya fue ingresado, por favor ingresa un nombre distinto');
+    } else if (!regex.test(nombreAmigos)) {
+        alert('Por favor, ingresa un nombre válido. No se permiten números ni símbolos');
+
+    } else {
+        amigos.push(nombreAmigos); // Agregar el nombre al array
     }
-    amigos.push(nombreAmigos); // Agregar el nombre al array
-    console.log(amigos);
-    limpiarCaja();
+
+    limpiarCaja('amigo');
     listarAmigos();
+    return;
 }
 
-function limpiarCaja() {
-    let valorCaja = document.querySelector("#amigo").value = "";
-}
 
 function listarAmigos() {
-    listaAmigos = document.getElementById("listaAmigos");
+    let listaAmigos = document.getElementById("listaAmigos");
     listaAmigos.innerHTML = ""; // Limpiar la lista antes de agregar nuevos elementos
 
     for (let i = 0; i < amigos.length; i++) {
         listaAmigos.innerHTML += `<li>${amigos[i]}</li>`;
     }
+    return;
 }
 
 function sortearAmigo() {
+    let sorteo = document.getElementById('resultado');
+    sorteo.innerHTML = '';
+
+
     if (amigos.length === 0) {
         alert("no hay amigos en la lista para sortear");
-        return;
-    }
+    } else {
 
-    let IndiceAleatorio = Math.floor(Math.random() * amigos.length)
-    amigoSorteado = amigos[IndiceAleatorio];
-    document.getElementById("resultado").innerHTML = `🎉 El amigo secreto sorteado es: ${amigoSorteado}🎉`;
+        amigoSorteado = amigos[IndiceAleatorio()];
+
+        if (amigos.length === listaSorteados.length) {
+            alert('Todos los amigos han sido sorteados, inicie de nuevo')
+            reiniciarSorteo();
+        } else {
+
+            //mientras en la lista de sorteados este incluido el amigo sorteado 
+            // accedemos para obtnener otro amigo sorteado
+            while (listaSorteados.includes(amigoSorteado)) {
+                amigoSorteado = amigos[IndiceAleatorio()];
+            }
+
+
+            let sorteado = (`El amigo secreto sorteado es: ${amigoSorteado}`);
+            let li = document.createElement('li');
+            li.textContent = sorteado
+            sorteo.appendChild(li);
+            listaSorteados.push(amigoSorteado);
+        }
+
+        let listaAmigos = document.getElementById('listaAmigos');
+        listaAmigos.innerHTML = '';
+
+    }
+    return;
 }
+
+
+function IndiceAleatorio() {
+    let aleatorio = Math.floor(Math.random() * amigos.length);
+    return aleatorio;
+}
+
+function limpiarCaja(elemento) {
+    document.getElementById(elemento).value = '';
+    return;
+}
+
+function reiniciarSorteo() {
+    amigos = [];         //Limpiar la lista de amigos
+    listaSorteados = [];    // Limpiar la lista de sorteados
+
+    // Limpiar la interfaz para que quede vacía
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = '';
+
+    let sorteoResultado = document.getElementById('resultado');
+    sorteoResultado.innerHTML = '';
+    return;
+}
+
+
+
